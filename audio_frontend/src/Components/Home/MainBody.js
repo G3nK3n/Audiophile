4ed82efdff8ea2bfd0d3eebc6ee0c798189b1a32
G3nK3n <ken_serrano@Hotmail.com>
2023-03-17 14:39:31 -0400

    Started Project

---
+++ audio_frontend/src/Components/Home/MainBody.js
@@ -0,0 +1,37 @@
+import React from "react";
+import classes from './MainBody.module.css';
+
+import Container from "react-bootstrap/Container";
+import { Row, Col, Button } from "react-bootstrap";
+
+import Img1 from '../../image-header.jpg';
+
+
+const MainBody = () => {
+    return(
+        <div className={classes.MainBody}>
+            <Container className={classes.HomeContainer}>
+                <Row>
+                    <Col>
+                        <div>
+                            <p>New Product</p>
+                            <h1>XX99 MARK II <br/> HEADPHONES</h1>
+                            <p>
+                                Experience natural, lifelike audio and exceptional build quality
+                                made for the passionate music enthusiast.
+                            </p>
+                            <Button>See Product</Button>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className={classes.HeadphonesImage}>
+                            
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default MainBody;
---
+++ audio_frontend/src/Components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MainBody from './Home/MainBody';
+
+const Layout = () => {
+    return(
+        <div>
+            <MainBody />
+        </div>
+    )
+}
+
+export default Layout;
