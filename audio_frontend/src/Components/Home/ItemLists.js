6b2614e7aba6ab7396601ec9f0d063206ead908a
G3nK3n <ken_serrano@Hotmail.com>
2023-03-22 23:28:59 -0400

    ItemsLists fixes

--- audio_frontend/src/Components/Home/ItemLists.js
+++ audio_frontend/src/Components/Home/ItemLists.js
@@ -6,6 +6,8 @@ import { Row, Col, Button } from "react-bootstrap";
 
 import RightArrowIcon from '../../icon-arrow-right.svg';
 import HeadphonesThumbnail from '../../image-category-thumbnail-headphones.png';
+import SpeakersThumbnail from '../../image-category-thumbnail-speakers.png';
+import EarphonesThumbnail from '../../image-category-thumbnail-earphones.png';
 
 const ItemList = () => {
     return(
@@ -15,12 +17,33 @@ const ItemList = () => {
                     <Col>
                         <div>
                             <div className={classes.ItemsLists}>
+                                <img className={classes.ItemsListsHeadphones} src={HeadphonesThumbnail} />
                                 <div className={classes.ItemsListsInner}>
-                                    {/* <img className={classes.ItemsListsThumb} src={HeadphonesThumbnail} /> */}
                                     <h3>Headphones</h3>
                                     <a href="#">Shop<img className={classes.ArrowIcon} src={RightArrowIcon} /></a>
                                 </div>
                             </div>
+                            <div className={classes.ItemsLists}>
+                                <img className={classes.ItemsListsSpeakers} src={SpeakersThumbnail} />
+                                <div className={classes.ItemsListsInner}>
+                                    <h3>Speakers</h3>
+                                    <a href="#">Shop<img className={classes.ArrowIcon} src={RightArrowIcon} /></a>
+                                </div>
+                            </div>
+                            <div className={classes.ItemsLists}>
+                                <img className={classes.ItemsListsEarphones} src={EarphonesThumbnail} />
+                                <div className={classes.ItemsListsInner}>
+                                    <h3>Earphones</h3>
+                                    <a href="#">Shop<img className={classes.ArrowIcon} src={RightArrowIcon} /></a>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className={classes.ZX9SpeakerSection}>
+
                         </div>
                     </Col>
                 </Row>
